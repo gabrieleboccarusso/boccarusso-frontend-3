e070479a0e7b29e3bfeffb7fb9f49f4e4e0075d4
gabrieleboccarusso <boccarussodev@gmail.com>
2023-02-23 14:17:41 +0100

    divided components about-me and articles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SkillsComponent } from './bases/skills/skills.component';
+import { ArticlesComponent } from './bases/articles/articles.component';
+import { AboutMeComponent } from './bases/about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SkillsComponent
+    SkillsComponent,
+    ArticlesComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
