1cfe8efb7a58f32abc6af5a2d763f9914c552c32
gabrieleboccarusso <boccarussodev@gmail.com>
2023-02-24 20:24:32 +0100

    made api sorter service methods

--- src/app/data/services/apiSorterService/api-sorter.service.ts
+++ src/app/data/services/apiSorterService/api-sorter.service.ts
@@ -4,6 +4,34 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiSorterService {
+  domain: string = "http://localhost:8080";
+  tag_url: string = this.domain + "/tag";
+  project_url: string = this.domain + "/project";
+  article_url: string = this.domain + "/article";
 
   constructor() { }
-}
+
+  public allTagsURL(): string {
+    return this.tag_url;
+  }
+
+  public allProjectsURL(): string{
+    return this.project_url;
+  }
+
+  public allArticlesURL(): string {
+    return this.article_url;
+  }
+
+  public articleBySlug(slug: string) {
+    return this.article_url + '/' + slug;
+  }
+
+  public articleByTitle(title: string): string {
+    return this.article_url + "/title?value=" + title;
+  }
+
+  public articleByTag(tag: string): string {
+    return this.article_url + "/tag?value=" + tag;
+  }
+ }
