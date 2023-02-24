c494fae46b79ce6906327b19e720ed6e3bec01f1
gabrieleboccarusso <boccarussodev@gmail.com>
2023-02-24 18:23:24 +0100

    adjusted tag component query

--- src/app/tags/tags.component.ts
+++ src/app/tags/tags.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Tag } from '../data/interfaces/tag';
 import { TagService } from '../data/services/tagService/tag.service';
 
 @Component({
@@ -7,12 +8,13 @@ import { TagService } from '../data/services/tagService/tag.service';
   styleUrls: ['./tags.component.css']
 })
 export class TagsComponent {
+tags?: Tag[];
+
   constructor(
     private tagService: TagService
   ) {}
 
   ngOnInit() {
-
-    this.tagService.getAllTags().subscribe(x => console.log(x));
+    this.tagService.getAllTags().subscribe(x => this.tags = x);
   }
 }
