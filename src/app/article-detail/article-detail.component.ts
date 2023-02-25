9c732fe41a71630432dd31907edad0b946f32395
gabrieleboccarusso <boccarussodev@gmail.com>
2023-02-25 04:44:31 +0100

    fixed tag field name in article interface

--- src/app/article-detail/article-detail.component.ts
+++ src/app/article-detail/article-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Article } from '../data/interfaces/article';
+import { ArticleService } from '../data/services/articleService/article.service';
 
 @Component({
   selector: 'app-article-detail',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./article-detail.component.css']
 })
 export class ArticleDetailComponent {
+  article?: Article;
+  content?: string;
 
-}
+  constructor (
+    private route: ActivatedRoute,
+    private articleService: ArticleService
+  ) {}
+
+  ngOnInit() {
+    let slug: string = this.route.snapshot.paramMap.get("slug") || "";
+    let content_url: string;
+    this.articleService.getArticleBySlug(slug).subscribe(x => {
+      this.article = x;
+      console.log(x);
+      content_url = this.article?.content || "";
+      this.articleService.getContent(content_url).subscribe(x => this.content = x);
+})};
+
+  }
