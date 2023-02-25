1cea028efb969a976e1eb3b2b9414c4874fa76c9
gabrieleboccarusso <boccarussodev@gmail.com>
2023-02-25 02:01:49 +0100

    got all articles in component

--- src/app/home/articles/articles.component.ts
+++ src/app/home/articles/articles.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Article } from 'src/app/data/interfaces/article';
+import { ArticleService } from 'src/app/data/services/articleService/article.service';
 
 @Component({
   selector: 'app-articles',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./articles.component.css']
 })
 export class ArticlesComponent {
+  articles?: Article[];
 
+  constructor (
+    private articleService: ArticleService
+  ) {}
+
+  ngOnInit() {
+    this.articleService.getAllArticles().subscribe(x => this.articles = x);
+  }
 }
