c7713db0264519b59c1a1a6aa68d1e9b8de4fc59
gabrieleboccarusso <boccarussodev@gmail.com>
2023-02-25 01:44:01 +0100

    added get all articles in related service

--- src/app/data/services/articleService/article.service.ts
+++ src/app/data/services/articleService/article.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Article } from '../../interfaces/article';
+import { ApiSorterService } from '../apiSorterService/api-sorter.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
 
-  constructor() { }
+  constructor(
+    private api: ApiSorterService,
+    private http: HttpClient
+  ) { }
+
+  public getAllArticles(): Observable<Article[]> {
+    return this.http.get<Article[]>(this.api.allArticlesURL());
+  }
 }
