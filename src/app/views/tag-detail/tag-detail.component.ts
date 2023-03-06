4d3bd158a5f8f87562db28f373efe0280dc679c0
gabrieleboccarusso <boccarussodev@gmail.com>
2023-03-06 18:31:31 +0000

    updated tag detail observable

--- src/app/views/tag-detail/tag-detail.component.ts
+++ src/app/views/tag-detail/tag-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { UtilitiesService } from 'src/app/logic/services/utilitiesService/utilities.service';
 import { Article } from '../../logic/interfaces/article';
 import { ArticleService } from '../../logic/services/articleService/article.service';
 
@@ -9,17 +10,25 @@ import { ArticleService } from '../../logic/services/articleService/article.serv
   styleUrls: ['./tag-detail.component.css']
 })
 export class TagDetailComponent {
+  loading: boolean = true;
   tag?: string;
   articles?: Article[];
 
   constructor (
     private route: ActivatedRoute,
-    private articleService: ArticleService
+    private articleService: ArticleService,
+    private utilities: UtilitiesService
   ) {}
 
   ngOnInit() {
     this.tag = this.route.snapshot.paramMap.get("slug") || "";
-    this.articleService.getArticlesByTag(this.tag).subscribe(x => this.articles = x);
+    this.articleService.getArticlesByTag(this.tag).subscribe({
+      next: x => {
+        this.loading = false;
+        this.articles = x;
+      },
+      error: err => this.utilities.onError(err)
+    });
     this.tag = this.unsanitize(this.tag);
   }
 
