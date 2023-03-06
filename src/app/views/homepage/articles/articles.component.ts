ae7e9656a3e936b4d207e604b31dbf4f236126cc
gabrieleboccarusso <boccarussodev@gmail.com>
2023-03-06 19:15:22 +0000

    upgraded observable in articles

--- src/app/views/homepage/articles/articles.component.ts
+++ src/app/views/homepage/articles/articles.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Article } from 'src/app/logic/interfaces/article';
 import { ArticleService } from 'src/app/logic/services/articleService/article.service';
+import { UtilitiesService } from 'src/app/logic/services/utilitiesService/utilities.service';
 
 @Component({
   selector: 'app-articles',
@@ -8,6 +9,7 @@ import { ArticleService } from 'src/app/logic/services/articleService/article.se
   styleUrls: ['./articles.component.css']
 })
 export class ArticlesComponent {
+  loading: boolean = true;
   articles: Article[] = [];
   totalNumArticles: number = 0;
   currentPage: number = 0;
@@ -15,22 +17,27 @@ export class ArticlesComponent {
   noMoreArticles: boolean = false;
 
   constructor (
+    private utilities: UtilitiesService,
     private articleService: ArticleService
   ) {}
   
   ngOnInit() {
-    this.articleService.getArticlesTotalNum().subscribe(x => {
-      this.totalNumArticles = x;
-
-      this.articleService.getPagingArticles(this.currentPage, this.pageSize).subscribe(x => {
-        x.forEach(article => this.articles.push(article))
-      });
-      this.currentPage++;
-      this.totalNumArticles -= this.pageSize;
-
-      if (this.totalNumArticles <= 0) {
-        this.noMoreArticles = true;
-      }
+    this.articleService.getArticlesTotalNum().subscribe({
+      next: x => {
+        this.loading = false;
+        this.totalNumArticles = x;
+  
+        this.articleService.getPagingArticles(this.currentPage, this.pageSize).subscribe(x => {
+          x.forEach(article => this.articles.push(article))
+        });
+        this.currentPage++;
+        this.totalNumArticles -= this.pageSize;
+  
+        if (this.totalNumArticles <= 0) {
+          this.noMoreArticles = true;
+        }
+      },
+      error: err => this.utilities.onError(err)
     });
   }
 
