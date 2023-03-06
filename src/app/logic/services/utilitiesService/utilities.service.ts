ced4edbb7ca1566c0046ac80e8d6e2ee7b2e8361
gabrieleboccarusso <boccarussodev@gmail.com>
2023-03-06 20:35:58 +0000

    made meta tags setter and keyword string maker in utilities

--- src/app/logic/services/utilitiesService/utilities.service.ts
+++ src/app/logic/services/utilitiesService/utilities.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { Meta } from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root'
@@ -7,8 +9,46 @@ import { Router } from '@angular/router';
 export class UtilitiesService {
 
   constructor(
-    private router: Router
-  ) { }
+    private router: Router,
+    private metaTags: Meta,
+    private title: Title
+  ) {}
+
+  private makeKeywordsString(tags: string[]) {
+    let result: string = "";
+
+    for (let i: number = 0; i < tags.length-1; ++i) {
+      result += tags.at(i) + ', ';
+    }
+    result += tags.at(tags.length-1);
+
+    return result;
+  }
+
+  public setHeadData(
+    title: string,
+    description: string,
+    url: string,
+    image: string,
+    type: string,
+    tags: string[]
+  ) {
+
+    this.title.setTitle(`${title} | Gabriele Boccarusso`);
+    this.metaTags.addTags([
+      { name: 'author', content: 'Gabriele Boccarusso' },
+      { name: 'description', content: description },
+      { name: 'keywords', content: this.makeKeywordsString(tags) },
+      { name: 'og:url', content: url },
+      { name: 'canonical', content: url },
+      { name: 'og:type', content: type },
+      { name: 'og:title', content: title },
+      { name: 'og:image', content: image },
+      { name: 'og:image:type', content: 'image/jpeg' },
+      { name: 'og:locale', content: 'en_US'},
+      { name: 'og:image:alt', content: 'picture of author and creator of content Gabriele Boccarusso'}
+    ]);
+  }
 
   public onError(err: any) {
     console.log("error: " + err.status);
@@ -16,4 +56,6 @@ export class UtilitiesService {
       this.router['navigate'](['/404']);
     }
   }
+
+
 }
