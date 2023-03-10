59affba62dc20d297dceda4a1ee56fd7a26da6de
gabrieleboccarusso <boccarussodev@gmail.com>
2023-03-10 20:46:55 +0000

    added meta tags in article-detail

--- src/app/views/article-detail/article-detail.component.ts
+++ src/app/views/article-detail/article-detail.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Article } from '../../logic/interfaces/article';
 import { ArticleService } from '../../logic/services/articleService/article.service';
-import { Router } from '@angular/router';
 import { UtilitiesService } from 'src/app/logic/services/utilitiesService/utilities.service';
 
 @Component({
@@ -13,10 +12,10 @@ import { UtilitiesService } from 'src/app/logic/services/utilitiesService/utilit
 export class ArticleDetailComponent {
   loading: boolean = true;
   article?: Article;
+  image_alt?: string;
   content?: string;
 
   constructor (
-    private router: Router,
     private route: ActivatedRoute,
     private articleService: ArticleService,
     private utilities: UtilitiesService
@@ -24,15 +23,25 @@ export class ArticleDetailComponent {
 
   ngOnInit() {
     let slug: string = this.route.snapshot.paramMap.get("slug") || "";
-    let content_url: string;
     this.articleService.getArticleBySlug(slug).subscribe({
-      next: x => {
-        this.loading = false;
-        this.article = x;
-        content_url = this.article.content;
-        this.articleService.getContent(content_url).subscribe(x => this.content = x);
-      },
+      next: x => this.onArticle(x),
       error: err => this.utilities.onError(err)
     })
+  }
+
+  onArticle(article: Article) {
+    this.loading = false;
+    this.article = article;
+    this.image_alt = `main picture of Gabriele Boccarusso's article ${article.title}`;
+    this.articleService.getContent(this.article.content)
+      .subscribe(x => this.content = x);
+
+    this.utilities.setHeadData(
+      article.title,
+      article.tagline,
+      article.tag.map(t => {return t['name']}),
+      article.image,
+      this.image_alt
+    );
   }
   };
