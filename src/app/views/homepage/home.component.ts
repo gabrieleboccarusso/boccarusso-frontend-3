f568338fa6387b55be6090c5d96564f60d7efc0c
gabrieleboccarusso <boccarussodev@gmail.com>
2023-03-10 20:07:08 +0000

    called head data into home component and improved getting of profile picture source and alt text

--- src/app/views/homepage/home.component.ts
+++ src/app/views/homepage/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UtilitiesService } from 'src/app/logic/services/utilitiesService/utilities.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  pfp_src: string;
+  pfp_alt: string;
 
+  constructor(
+    private utilities: UtilitiesService,
+  ) {
+    this.pfp_src = utilities.getProfilePictureSource();
+    this.pfp_alt = utilities.getProfilePictureAlt();
+  }
+
+  ngOnInit() {
+    this.utilities.setHeadData(
+      "homepage",
+      "homepage of Gabriele Boccarusso's official website",
+      ['Java', 'Spring Boot', 'REST', 'API', 'Microservices', 'Software Architecture', 'Databases']
+    );
+  }
 }
